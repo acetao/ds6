a3d826378fea9fb35e697dc401a7d829007a5da7
acetao <acetao@13.com>
2022-08-07 18:14:57 +0800

    单链表选择排序算法

--- ch2/selection_sort_linkedlist.cpp
+++ ch2/selection_sort_linkedlist.cpp
@@ -4,15 +4,16 @@
 void Sort(LinkNode* h)
 {
 	if (h->next == NULL || h->next->next == NULL)
-		return;								//空表或者只有一个结点时返回
+		return;					//空表或者只有一个结点时返回
 	LinkNode *p, *minp, *q;
 	int minv;
 	p = h->next;
-	while (p != NULL)						//用指针p遍历h的所有结点
+	while (p != NULL)			//用指针p遍历h的所有结点
 	{
 		minv = p->data;
 		minp = p;
-		for (q = p->next; q != NULL; q = q->next)	//从结点p开始找到最小值的结点minp
+		//从结点p开始找到最小值的结点minp
+		for (q = p->next; q != NULL; q = q->next)
 			if (q->data < minv)
 			{
 				minp = q;
@@ -20,7 +21,7 @@ void Sort(LinkNode* h)
 			}
 		if (minp != p)
 		{
-			int tmp = p->data;				//交换结点p和minp的结点值
+			int tmp = p->data;	//交换结点p和minp的结点值
 			p->data = minp->data;
 			minp->data = tmp;
 		}
