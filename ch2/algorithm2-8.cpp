d6c4e7d62874b4e578ac623b2edb6f1b1bd1526d
acetao <acetao@13.com>
2022-08-07 18:20:18 +0800

    【例2.8】的算法:单链表L递增（插入）排序

--- ch2/algorithm2-8.cpp
+++ ch2/algorithm2-8.cpp
@@ -1,18 +1,18 @@
-//【例2.8】的算法:单链表L递增排序
+//【例2.8】的算法:单链表L递增（插入）排序
 #include "linklist.cpp"
 
 void sort(LinkNode *&L)
 {
 	LinkNode *p, *pre, *q;
 	p = L->next->next;		//p指向L的第2个数据结点
-	L->next->next = NULL;		//构造只含一个数据结点的有序表
+	L->next->next = NULL;	//构造只含一个数据结点的有序表
 	while (p != NULL)
 	{
-		q = p->next;			//q保存p结点后继结点的指针
-		pre = L;				//从有序表开头进行比较,pre指向p结点的前驱结点
+		q = p->next;		//q保存p结点后继结点的指针
+		pre = L;			//从有序表开头进行比较,pre指向p结点的前驱结点
 		while (pre->next != NULL && pre->next->data < p->data)
-			pre = pre->next;	//在有序表中找插入p结点的前驱结点pre
-		p->next = pre->next;	//在pre结点之后插入p结点
+			pre = pre->next;//在有序表中找插入p结点的前驱结点pre
+		p->next = pre->next;//在pre结点之后插入p结点
 		pre->next = p;
 		p = q;				//扫描原单链表余下的结点
 	}
@@ -29,6 +29,6 @@ int main()
 	sort(L);
 	printf("L:"); DispList(L);
 	DestroyList(L);
-	return 1;
+	return 0;
 }
 
